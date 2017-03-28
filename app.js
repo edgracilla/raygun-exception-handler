9c3f12301b9c84ee06b2a4c653180d8f789f6109
edgracilla <ed.gracilla@gmail.com>
2017-03-28 17:58:32 +0800

    update

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 'use strict'
 
 const reekoh = require('reekoh')
-const _plugin = new reekoh.plugins.ExceptionLogger()
+const plugin = new reekoh.plugins.ExceptionLogger()
 
 let raygunClient =  null
 
-_plugin.on('exception', (error) => {
+plugin.on('exception', (error) => {
   raygunClient.send(error, {}, (response) => {
     if (response.statusCode === 202) return
 
     console.error('Error on Raygun.', response.statusMessage)
-    _plugin.logException(new Error(response.statusMessage))
+    plugin.logException(new Error(response.statusMessage))
   })
-  _plugin.log(JSON.stringify({
+  plugin.log(JSON.stringify({
     title: 'Exception sent to Raygun',
     data: {message: error.message, stack: error.stack, name: error.name}
   }))
 })
 
-_plugin.once('ready', () => {
+plugin.once('ready', () => {
   let raygun = require('raygun')
-  raygunClient = new raygun.Client().init({ apiKey: _plugin.config.apiKey})
+  raygunClient = new raygun.Client().init({ apiKey: plugin.config.apiKey})
 
-  _plugin.log('Raygun Exception Handler Initialized.')
-  _plugin.emit('init')
+  plugin.log('Raygun Exception Handler Initialized.')
+  plugin.emit('init')
 })
 
-module.exports = _plugin
+module.exports = plugin
